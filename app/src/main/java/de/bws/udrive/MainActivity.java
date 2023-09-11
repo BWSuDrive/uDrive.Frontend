6b6a4c36b19a3f384f09edef0c1f02a8105116aa
Fabian Haßa <skilector01@gmail.com>
2023-09-11 16:17:44 +0200

    Login Page mit Failed und Successsss

--- app/src/main/java/de/bws/udrive/MainActivity.java
+++ app/src/main/java/de/bws/udrive/MainActivity.java
@@ -2,13 +2,44 @@ package de.bws.udrive;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.service.autofill.OnClickAction;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText username;
+
+    private EditText password;
+
+    private Button loginButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        username = findViewById(R.id.username);
+        password = findViewById(R.id.password);
+        loginButton = findViewById(R.id.loginbutton);
+
+        loginButton.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+                if(username.getText().toString().equals("admin") && password.getText().toString().equals("123")) {
+                    Toast.makeText(MainActivity.this, "Login Successfull", Toast.LENGTH_SHORT).show();
+                    //          Intent Home = new Intent(MainActivity.this, HomeActivity.class);
+                    //          Home.putExtra("username", username.getText().toString());
+                    //          MainActivity.this.startActivity(Home);
+                } else {
+                    Toast.makeText(MainActivity.this, "Login Failed", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
     }
 }
