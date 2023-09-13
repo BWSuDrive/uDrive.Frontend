f1684448657e729164a4cfd3e6e759f9ee9914e9
fabianhassa <skilector01@gmail.com>
2023-09-13 09:51:10 +0200

    Logo hinzugefügt, Namen bearbeitet, Warning Popup gebaut

--- app/src/main/java/de/bws/udrive/MainActivity.java
+++ app/src/main/java/de/bws/udrive/MainActivity.java
@@ -1,10 +1,14 @@
 package de.bws.udrive;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
 
 import android.content.Intent;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 import android.service.autofill.OnClickAction;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -32,8 +36,7 @@ public class MainActivity extends AppCompatActivity {
     /**
      * Initialisiert die Elemente auf der UI per ID
      */
-    private void initElements()
-    {
+    private void initElements() {
         this.username = findViewById(R.id.username);
         this.password = findViewById(R.id.password);
         this.loginButton = findViewById(R.id.loginbutton);
@@ -41,9 +44,8 @@ public class MainActivity extends AppCompatActivity {
 
     /**
      * Setzt die Listener für einzelne Objekte
-     * */
-    private void handleListeners()
-    {
+     */
+    private void handleListeners() {
         loginButton.setOnClickListener(loginButtonListener);
     }
 
@@ -51,18 +53,38 @@ public class MainActivity extends AppCompatActivity {
         @Override
         public void onClick(View view) {
             // Wenn Username und Passwort übereinstimmen mach ein Toast (unten kleiner Popup) der sagt was passiert ist
-            if(username.getText().toString().equals("admin") && password.getText().toString().equals("123"))
-            {
+            if (username.getText().toString().equals("admin") && password.getText().toString().equals("123")) {
                 Toast.makeText(MainActivity.this, "Login Successfull", Toast.LENGTH_SHORT).show();
 
                 Intent homeIntent = new Intent(MainActivity.this, HomeActivity.class);
                 homeIntent.putExtra("username", username.getText().toString());
                 MainActivity.this.startActivity(homeIntent);
-            }
-            else
-            {
+            } else {
+                showErrorDialog();
                 Toast.makeText(MainActivity.this, "Login Failed", Toast.LENGTH_SHORT).show();
             }
         }
     };
+
+    private void showErrorDialog() {
+        ConstraintLayout errorLayout = findViewById(R.id.errorConstraintLayout);
+        View view = LayoutInflater.from(MainActivity.this).inflate(R.layout.error_dialog, errorLayout);
+        Button errorClose = view.findViewById(R.id.errorClose);
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+        builder.setView(view);
+        final AlertDialog alertDialog = builder.create();
+
+        errorClose.findViewById(R.id.errorClose).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                alertDialog.dismiss();
+                Toast.makeText(MainActivity.this, "Close", Toast.LENGTH_SHORT).show();
+            }
+        });
+        if (alertDialog.getWindow() !=null){
+            alertDialog.getWindow().setBackgroundDrawable(new ColorDrawable(0));
+        }
+        alertDialog.show();
+    }
 }
