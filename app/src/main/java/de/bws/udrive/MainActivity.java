94c9ae030cb0749846dc91725ec6f24a892ed7bd
Lucas Christian <lloyd.hd05@gmail.com>
2023-09-22 14:07:35 +0200

    Changes to doc and reorganized code

--- app/src/main/java/de/bws/udrive/MainActivity.java
+++ app/src/main/java/de/bws/udrive/MainActivity.java
@@ -24,61 +24,72 @@ import de.bws.udrive.utilities.model.uDrive;
 import de.bws.udrive.utilities.model.uDriveHandler;
 
 /**
- * Login-Activity (App wird hier gestartet)
- * Öffnet bei erfolgreichem Login die {@link HomeActivity}
+ * Login-Activity (App wird hier gestartet) <br>
+ * Öffnet bei erfolgreichem Login oder erfolgreicher Registrierung die {@link HomeActivity}
  */
 public class MainActivity extends AppCompatActivity {
 
     /* UI Elemente */
-
     private TabLayout tabLayout;
     private ViewPager2 viewPager2;
     private ViewPagerAdapter adapter;
 
 
-    /* "Konstruktor"-Methode */
+    /**
+     * Konstruktor-Methode für MainActivity <br>
+     * Zeigt den Login & Registrierungs Screen an <br>
+     *
+     * @author Fabian
+     * */
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-
+        /* Initialisierung von Objekten */
         tabLayout = findViewById(R.id.tab_layout);
         viewPager2 = findViewById(R.id.view_pager);
 
+        /* Neue Tabs hinzufügen */
         tabLayout.addTab(tabLayout.newTab().setText("Login"));
         tabLayout.addTab(tabLayout.newTab().setText("Signup"));
 
+        /* Adapter setzen */
         FragmentManager fragmentManager = getSupportFragmentManager();
         adapter = new ViewPagerAdapter(fragmentManager, getLifecycle());
         viewPager2.setAdapter(adapter);
 
-        tabLayout.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
-            @Override
-            public void onTabSelected(TabLayout.Tab tab) {
-                viewPager2.setCurrentItem(tab.getPosition());
-            }
-
-            @Override
-            public void onTabUnselected(TabLayout.Tab tab) {
-
-            }
-
-            @Override
-            public void onTabReselected(TabLayout.Tab tab) {
-
-            }
-        });
-
-        viewPager2.registerOnPageChangeCallback(new ViewPager2.OnPageChangeCallback() {
-            @Override
-            public void onPageSelected(int position) {
-                tabLayout.selectTab(tabLayout.getTabAt(position));
-            }
-        });
+        /* Listener & Callbacks setzen */
+        tabLayout.addOnTabSelectedListener(tabSelectedListener);
+        viewPager2.registerOnPageChangeCallback(onPageChange);
     }
 
     /* =================================================================================== */
+
+    /**
+     * OnTabSelectedListener für tabLayout Objekt
+     *
+     * @author Fabian
+     */
+    private final TabLayout.OnTabSelectedListener tabSelectedListener = new TabLayout.OnTabSelectedListener()
+    {
+        @Override
+        public void onTabSelected(TabLayout.Tab tab) { viewPager2.setCurrentItem(tab.getPosition()); }
+        @Override
+        public void onTabUnselected(TabLayout.Tab tab) { }
+        @Override
+        public void onTabReselected(TabLayout.Tab tab) { }
+    };
+
+    /**
+     * OnPageChangeCallback für viewPager2 Objekt
+     *
+     * @author Fabian
+     */
+    private final ViewPager2.OnPageChangeCallback onPageChange = new ViewPager2.OnPageChangeCallback() {
+        @Override
+        public void onPageSelected(int position) { tabLayout.selectTab(tabLayout.getTabAt(position)); }
+    };
 
 }
