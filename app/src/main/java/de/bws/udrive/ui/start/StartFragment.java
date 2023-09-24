b24bf84921fd4d9c91bcdeb0d50229b26d2b8685
Fabian <77246896+Skilecto@users.noreply.github.com>
2023-09-24 21:22:38 +0200

    onlyforme

--- app/src/main/java/de/bws/udrive/ui/start/StartFragment.java
+++ app/src/main/java/de/bws/udrive/ui/start/StartFragment.java
@@ -10,11 +10,107 @@ import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import de.bws.udrive.R;
 import de.bws.udrive.databinding.FragmentStartBinding;
 
 public class StartFragment extends Fragment {
 
     private FragmentStartBinding binding;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        RecyclerView recyclerView = null;
+
+        recyclerView = recyclerView.findViewById(R.id.recycler_view);
+        List<ItemModel> itemList = new ArrayList<>();
+        itemList.add(new ItemModel("Objekt 1", "Beschreibung 1"));
+        itemList.add(new ItemModel("Objekt 2", "Beschreibung 2"));
+        // Weitere Elemente hier hinzufügen
+
+        ItemAdapter adapter = new ItemAdapter(itemList);
+        recyclerView.setAdapter(adapter);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+    }
+
+    private void setContentView(int activityMain) {
+    }
+
+    class ItemModel {
+        private String name;
+        private String description;
+
+        public ItemModel(String name, String description) {
+            this.name = name;
+            this.description = description;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public String getDescription() {
+            return description;
+        }
+    }
+
+    class ItemAdapter extends RecyclerView.Adapter<ItemAdapter.ViewHolder> {
+        private List<ItemModel> itemList;
+
+        public ItemAdapter(List<ItemModel> itemList) {
+            this.itemList = itemList;
+        }
+
+        @NonNull
+        @Override
+        public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+            View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_layout, parent, false);
+            return new ViewHolder(view);
+        }
+
+        @Override
+        public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+            ItemModel item = itemList.get(position);
+            holder.nameTextView.setText(item.getName());
+            holder.descriptionTextView.setText(item.getDescription());
+        }
+
+        @Override
+        public int getItemCount() {
+            return itemList.size();
+        }
+
+        public class ViewHolder extends RecyclerView.ViewHolder {
+            TextView nameTextView;
+            TextView descriptionTextView;
+
+            public ViewHolder(View itemView) {
+                super(itemView);
+                nameTextView = itemView.findViewById(R.id.nameTextView);
+                descriptionTextView = itemView.findViewById(R.id.descriptionTextView);
+            }
+        }
+    }
+}
+
+
+/* Backup
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
@@ -28,10 +124,10 @@ public class StartFragment extends Fragment {
         startViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
         return root;
     }
+            @Override
+        public void onDestroyView() {
+            super.onDestroyView();
+            binding = null;
+        }
 
-    @Override
-    public void onDestroyView() {
-        super.onDestroyView();
-        binding = null;
-    }
-}
+ */
