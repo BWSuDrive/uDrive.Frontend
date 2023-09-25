36655967e6a705903fc90618f85cb93032fbee16
Lucas Christian <lloyd.hd05@gmail.com>
2023-09-25 11:35:20 +0200

    Added telephone field to SignUp class

--- app/src/main/java/de/bws/udrive/utilities/model/uDrive.java
+++ app/src/main/java/de/bws/udrive/utilities/model/uDrive.java
@@ -165,13 +165,15 @@ public class uDrive {
         private String Email;
         private String Password;
         private String UserName;
+        private String telephone;
 
-        public SignUp(String vorname, String nachname, String email, String password) {
+        public SignUp(String vorname, String nachname, String email, String password, String telephone) {
             this.Firstname = vorname;
             this.Lastname = nachname;
             this.Email = email;
             this.UserName = email;
             this.Password = password;
+            this.telephone = telephone;
         }
 
         public boolean equalPasswords(String confirmPassword) { return this.Password.contentEquals(confirmPassword); }
