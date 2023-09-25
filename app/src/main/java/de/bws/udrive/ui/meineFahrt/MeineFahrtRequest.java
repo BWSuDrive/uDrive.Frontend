225b0882b86b99b05f0154574593c32f2b1b4e28
Niko Löwen <99402353+n-loewen@users.noreply.github.com>
2023-09-25 12:06:35 +0200

    Class for drive Requests
    +Added getter / setter

--- app/src/main/java/de/bws/udrive/ui/meineFahrt/MeineFahrtRequest.java
+++ app/src/main/java/de/bws/udrive/ui/meineFahrt/MeineFahrtRequest.java
@@ -1,5 +1,40 @@
 package de.bws.udrive.ui.meineFahrt;
 
 public class MeineFahrtRequest {
+    String vorname;
+    String nachname;
+    String phone;
+    String freitext;
 
+    public String getVorname() {
+        return vorname;
+    }
+
+    public void setVorname(String vorname) {
+        this.vorname = vorname;
+    }
+
+    public String getNachname() {
+        return nachname;
+    }
+
+    public void setNachname(String nachname) {
+        this.nachname = nachname;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getFreitext() {
+        return freitext;
+    }
+
+    public void setFreitext(String freitext) {
+        this.freitext = freitext;
+    }
 }
