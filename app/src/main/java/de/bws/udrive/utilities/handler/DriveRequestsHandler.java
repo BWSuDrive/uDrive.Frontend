1da19dae88dd73087812e098a47f1c51902848ff
Lucas Christian <lloyd.hd05@gmail.com>
2023-09-27 11:54:20 +0200

    Implemented code to fetch data

--- app/src/main/java/de/bws/udrive/utilities/handler/DriveRequestsHandler.java
+++ app/src/main/java/de/bws/udrive/utilities/handler/DriveRequestsHandler.java
@@ -4,11 +4,21 @@ import android.util.Log;
 
 import androidx.lifecycle.MutableLiveData;
 
+import com.google.gson.Gson;
+import com.google.gson.internal.LinkedTreeMap;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import de.bws.udrive.ui.start.model.AvailableTours;
+import de.bws.udrive.ui.start.model.Person;
+import de.bws.udrive.ui.start.model.PlannedDrive;
 import de.bws.udrive.utilities.APIClient;
 import de.bws.udrive.utilities.APIInterface;
 import de.bws.udrive.utilities.model.DriveRequest;
 import de.bws.udrive.utilities.model.General;
 import de.bws.udrive.utilities.response.DriveRequestResponse;
+import de.bws.udrive.utilities.uDriveUtilities;
 import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
@@ -24,26 +34,69 @@ public class DriveRequestsHandler {
     private String informationString = "";
     private boolean driveRequestsAvailable = true;
     private MutableLiveData<Boolean> isFinished = new MutableLiveData<>(Boolean.FALSE);
+    private List<AvailableTours> availableTours = new ArrayList<AvailableTours>();;
 
-    public void handle(DriveRequest driveRequest) {
-        Call<DriveRequestResponse> driveRequestResponse =
+    public void handle(DriveRequest driveRequest)
+    {
+        Call<List<DriveRequestResponse>> driveRequestResponse =
                 APIClient.getAPI().create(APIInterface.class)
                         .getAvailableDrivers(General.getSignedInUser().getHTTPAuthHeader(), driveRequest);
+
+        Log.i(TAG, new Gson().toJson(driveRequest));
 
         driveRequestResponse.enqueue(driveRequestCallback);
     }
 
-    private final Callback<DriveRequestResponse> driveRequestCallback = new Callback<DriveRequestResponse>() {
+    private final Callback<List<DriveRequestResponse>> driveRequestCallback = new Callback<List<DriveRequestResponse>>() {
         @Override
-        public void onResponse(Call<DriveRequestResponse> call, Response<DriveRequestResponse> response) {
+        public void onResponse(Call<List<DriveRequestResponse>> call, Response<List<DriveRequestResponse>> response) {
 
             Log.i(TAG, "Response-Code: " + response.code());
             switch(response.code())
             {
                 /* OK */
                 case 200:
-                    Log.i(TAG, "Responsecode 200");
-                    Log.i(TAG, response.toString());
+
+                    if(response.body() != null)
+                    {
+                        Log.i(TAG, "Responsecode 200");
+                        Log.i(TAG, response.toString());
+
+                        List<DriveRequestResponse> responseList = response.body();
+
+                        responseList.forEach(tour -> {
+
+                            String distance = tour.getDistance();
+
+                            /* Personendaten */
+                            String firstName = tour.getPerson().get("firstname").toString();
+                            String lastName = tour.getPerson().get("lastname").toString();
+                            String email = tour.getPerson().get("email").toString();
+                            String phoneNumber = tour.getPerson().get("phoneNumber").toString();
+
+                            /* Tourdaten */
+                            String id = tour.getTourPlan().get("id").toString();
+                            String departure = uDriveUtilities.parseString(tour.getTourPlan().get("departure").toString());
+                            String eta = tour.getTourPlan().get("eta").toString();
+                            String start = tour.getTourPlan().get("start").toString();
+                            String destination = tour.getTourPlan().get("destination").toString();
+                            String message = tour.getTourPlan().get("message").toString();
+
+                            Person person = new Person(firstName, lastName, email, phoneNumber);
+                            PlannedDrive plannedDrive = new PlannedDrive(distance, id, departure, eta, start, destination, message);
+
+                            AvailableTours availableTour = new AvailableTours(person, plannedDrive);
+
+                            availableTours.add(availableTour);
+                        });
+                    }
+                    else
+                    {
+                        informationString = "Body ist null!";
+                        Log.wtf(TAG, "Responsebody ist null!");
+                    }
+
+
                     break;
 
                 /* NO CONTENT */
@@ -71,7 +124,7 @@ public class DriveRequestsHandler {
         }
 
         @Override
-        public void onFailure(Call<DriveRequestResponse> call, Throwable t)
+        public void onFailure(Call<List<DriveRequestResponse>> call, Throwable t)
         {
             Log.i(TAG, "Failure");
             Log.i(TAG, t.getMessage());
@@ -85,4 +138,6 @@ public class DriveRequestsHandler {
     public String getInformationString() { return this.informationString; }
 
     public boolean requestsAvailable() { return this.driveRequestsAvailable; }
+
+    public List<AvailableTours> getAvailableTours() { return availableTours; }
 }
